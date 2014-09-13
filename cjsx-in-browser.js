56b7531dca20d2823155253771fef8367039389a
Dan Fox <iamdanfox@gmail.com>
2014-09-13 02:54:38 -0700

    Include all three sources, why not!

--- cjsx-in-browser.js
+++ cjsx-in-browser.js
@@ -4,9 +4,9 @@
   var coffeeReactTransform = require('coffee-react-transform')
   var CoffeeScript = require('coffee-script')
 
-  function sourceMapComment(originalCJSX, transformedCJSX, sourceMap, fileName) {
-    sourceMap.sources = [fileName, fileName + "_transformed"];
-    sourceMap.sourcesContent = [originalCJSX, transformedCJSX];
+  function sourceMapComment(originalCJSX, transformedCJSX, javascript, sourceMap, fileName) {
+    sourceMap.sources = [fileName, fileName + "_transformed", fileName + "_compiled"];
+    sourceMap.sourcesContent = [originalCJSX, transformedCJSX, javascript];
 
     var base64 = btoa(JSON.stringify(sourceMap));
     var dataUri = 'data:application/json;charset=utf-8;base64,' + base64;
@@ -19,9 +19,10 @@
     var compilerResult = CoffeeScript.compile(coffee, {
       sourceMap: true
     });
+    var javascript = compilerResult.js;
     var v3SourceMap = JSON.parse(compilerResult.v3SourceMap);
 
-    eval(compilerResult.js + sourceMapComment(originalCJSX, coffee, v3SourceMap, "cjsx_" + i));
+    eval(javascript + sourceMapComment(originalCJSX, coffee, javascript, v3SourceMap, "cjsx" + i));
   }
 
   var scripts = document.querySelectorAll("script[type='text/cjsx']");
